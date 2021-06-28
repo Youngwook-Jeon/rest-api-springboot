6b35eeec5d657a644095fc17a5cd9ccafdfd246f
Young wook Jeon <ela87@naver.com>
2021-06-28 18:33:39 +0900

    Set the project

---
+++ src/test/java/com/young/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.young.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
