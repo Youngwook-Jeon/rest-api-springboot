72dffca7bf427ae6bf06128eaaa1fa61304f284e
Young wook Jeon <ela87@naver.com>
2021-07-06 13:51:05 +0900

    POST /api/events returns 201 status code

---
+++ src/main/java/com/young/springbootrestapi/events/EventRepository.java
@@ -0,0 +1,6 @@
+package com.young.springbootrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
--- src/test/java/com/young/springbootrestapi/events/EventControllerTests.java
+++ src/test/java/com/young/springbootrestapi/events/EventControllerTests.java
@@ -1,13 +1,20 @@
 package com.young.springbootrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,10 +23,35 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API dev with Spring boot")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 7, 5, 13, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 7, 6, 13, 30))
+                .beginEventDateTime(LocalDateTime.of(2021, 7, 10, 13, 30))
+                .endEventDateTime(LocalDateTime.of(2021, 7, 11, 13, 30))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(200)
+                .location("신촌 스터디 카페")
+                .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
         mockMvc.perform(post("/api/events/")
-                .contentType(MediaType.APPLICATION_JSON).accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
