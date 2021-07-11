a2adc9cbf33ae44e40afd9dfe51362ba3841fddf
Young wook Jeon <ela87@naver.com>
2021-07-11 12:43:56 +0900

    Add parameterized tests for the Event class

--- src/main/java/com/young/springbootrestapi/events/EventController.java
+++ src/main/java/com/young/springbootrestapi/events/EventController.java
@@ -36,6 +36,7 @@ public class EventController {
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
+        event.update();
         Event newEvent = this.eventRepository.save(event);
         // Location URI 만들기
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- src/test/java/com/young/springbootrestapi/events/EventTest.java
+++ src/test/java/com/young/springbootrestapi/events/EventTest.java
@@ -1,6 +1,11 @@
 package com.young.springbootrestapi.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +30,46 @@ class EventTest {
 
         assertEquals(event.getName(), name);
         assertEquals(event.getDescription(), description);
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        event.update();
+
+        assertEquals(event.isFree(), isFree);
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForTestOffline")
+    public void testOffline(String location, boolean isOffline) {
+        Event event = Event.builder()
+                .location(location)
+                .build();
+
+        event.update();
+
+        assertEquals(event.isOffline(), isOffline);
+    }
+
+    private static Stream<Arguments> paramsForTestFree() {
+        return Stream.of(
+                Arguments.of(0, 0, true),
+                Arguments.of(100, 0, false),
+                Arguments.of(0, 100, false),
+                Arguments.of(100, 200, false)
+        );
+    }
+
+    private static Stream<Arguments> paramsForTestOffline() {
+        return Stream.of(
+                Arguments.of("신촌 스터디 카페", true),
+                Arguments.of(null, false)
+        );
     }
 }
